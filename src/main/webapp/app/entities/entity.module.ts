3f7e333a8778a14e8f72a80ad4f1fc1595e0f280
adhawan325 <adhawan@shiplets.com>
2018-03-22 08:42:50 -0700

    add entities

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,21 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { CmasBidsBidModule } from './bid/bid.module';
-import { CmasBidsDepartmentModule } from './department/department.module';
-import { CmasBidsBidDocumentModule } from './bid-document/bid-document.module';
-import { CmasBidsVendorModule } from './vendor/vendor.module';
-import { CmasBidsCandidateModule } from './candidate/candidate.module';
-import { CmasBidsVendorUserModule } from './vendor-user/vendor-user.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        CmasBidsBidModule,
-        CmasBidsDepartmentModule,
-        CmasBidsBidDocumentModule,
-        CmasBidsVendorModule,
-        CmasBidsCandidateModule,
-        CmasBidsVendorUserModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
